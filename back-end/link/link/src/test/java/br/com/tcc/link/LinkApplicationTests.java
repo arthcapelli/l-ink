37726637946e11263edd7129a3cfb1bf3b4c60da
lanamenoti <alana.pires@acad.pucrs.br>
2022-04-25 17:10:18 -0300

    Adiciona estrutura inicial do projeto

---
+++ back-end/link/link/src/test/java/br/com/tcc/link/LinkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tcc.link;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
