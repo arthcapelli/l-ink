80626d8d63e18eece31858c99a4798e5ac783513
arthcapelli <arthcapelli@gmail.com>
2022-05-19 21:55:14 -0300

    Ajustes register-user screen

--- back-end/link/link/src/main/java/br/com/tcc/link/LinkApplication.java
+++ back-end/link/link/src/main/java/br/com/tcc/link/LinkApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LinkApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LinkApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LinkApplication.class, args);
+    }
 
 }
