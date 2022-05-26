a4b39dd5da31a2466cdf824f5204a89225aadd76
lanamenoti <alana.pires@acad.pucrs.br>
2022-05-25 22:13:56 -0300

    Ajusta layout do feed no front

--- front-end/l-ink/src/ui/components/feed/feed.component.js
+++ front-end/l-ink/src/ui/components/feed/feed.component.js
@@ -1,33 +1,21 @@
-/*import Box from "@material-ui/core/Box";
-import ImageList from "@material-ui/core/ImageList";
-import ImageListItem from "@material-ui/core/ImageListItem";
-import ImageListItemBar from "@material-ui/core/ImageListItemBar";
-import IconButton from "@material-ui/core/IconButton";
-*/
-import Box from "@mui/material/Box";
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
-import ImageListItemBar from "@mui/material/ImageListItemBar";
-import IconButton from "@mui/material/IconButton";
-import heart_blank from "../../../assets/icons/heart-blank.png";
-import { useLinkApi } from "../../../api";
-import { useState, useEffect } from "react";
-import { Post } from "../index";
-import "./style.css";
+import { useState, useEffect } from "react"
+import { useLinkApi } from "../../../api"
+import { Post } from "../index"
+import "./style.css"
 
 export function Feed() {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([])
 
-  const { getPosts } = useLinkApi();
+  const { getPosts } = useLinkApi()
 
   useEffect(() => {
     async function getApiPosts() {
-      const apiPosts = await getPosts();
-      setPosts(apiPosts);
+      const apiPosts = await getPosts()
+      setPosts(apiPosts)
     }
 
-    getApiPosts();
-  }, []);
+    getApiPosts()
+  }, [])
 
   return (
     <div className="feed-list-content">
@@ -37,5 +25,5 @@ export function Feed() {
         posts.map((post) => <Post item={post} key={post.id} />)
       )}
     </div>
-  );
+  )
 }
--- front-end/l-ink/src/ui/components/post/post.component.js
+++ front-end/l-ink/src/ui/components/post/post.component.js
@@ -1,20 +1,25 @@
-import "./style.css";
-import heart_blank from "../../../assets/icons/heart-blank.png";
+import "./style.css"
+import heart_blank from "../../../assets/icons/heart-blank.png"
 
 export function Post({ item }) {
-  const { id, userResponse, postImg } = item;
+  const { id, userResponse, postImg } = item
 
   return (
     <div className="feed-card-content" key={id}>
-      <div className="feed-card-img">
+      <div>
         <img className="card-img" src={postImg} alt={userResponse.name}></img>
       </div>
       <div className="feed-card-info">
-        {/* <div className="feed-favorite-icon"> */}
+        <div className="feed-card-author">
+          <img
+            src={userResponse.avatar}
+            alt="avatar"
+            className="avatar-img"
+          ></img>
+          <div>{userResponse.name}</div>
+        </div>
         <img src={heart_blank} alt="heart-icon" className="icon-img"></img>
-        {/* </div> */}
-        <div className="feed-card-author">{userResponse.name}</div>
       </div>
     </div>
-  );
+  )
 }
